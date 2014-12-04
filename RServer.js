aa9a87c5a2cbc44a803d4f45537a4bb7c1eb15e7
Fredrik Landström <fredrik.landstrom@compose.se>
2014-12-04 06:59:20 +0100

    initial commit

---
+++ RServer.js
@@ -0,0 +1,30 @@
+var serverhostport='8080';
+
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+ 
+
+app.get('*',function(req,res){
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log('request to : '+fullUrl);
+    console.log('data:')
+    console.log('----');
+    console.log(req.body);
+    res.send('OK');
+})
+
+app.post('*',function(req,res){
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log('request to : '+fullUrl);
+    console.log('data:')
+    console.log('----');
+    console.log(req.body);
+    res.send('OK');
+})
